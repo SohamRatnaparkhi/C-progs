9dc026a2d1402ee08d5253382574d449780b7692
Soham Ratnaparkhi <soham.ratnaparkhi@gmail.com>
2022-03-11 11:53:26 +0530

    PSAP lab

--- PSAP/LabAssignments/recursion/binarysearch.c
+++ PSAP/LabAssignments/recursion/binarysearch.c
@@ -1,35 +1,43 @@
 #include <stdio.h>
-
-int binarySearch(int arr[], int l, int r, int x)
+int binary_search(int num[], int s, int e, int target)
 {
-    if (r >= l)
+    while (e >= s)
     {
-        int mid = l + (r - l) / 2;
-
-        // If the element is present at the middle itself
-        if (arr[mid] == x)
+        int mid = s + ((e - s) / 2);
+        if (num[mid] == target)
             return mid;
-
-        // If element is smaller than mid, then it can only be present
-        // in left subarray
-        if (arr[mid] > x)
-            return binarySearch(arr, l, mid - 1, x);
-
-        // Else the element can only be present in right subarray
-        return binarySearch(arr, mid + 1, r, x);
+        else if (num[mid] > target)
+        {
+            // go left to check for target
+            // e = mid - 1;
+            return binary_search(num, s, mid - 1, target);
+        }
+        else if (num[mid] < target)
+        {
+            // go right
+            //s = mid + 1;
+            return binary_search(num, mid + 1, e, target);
+        }
     }
-
-    // We reach here when element is not present in array
     return -1;
 }
-
-int main(void)
+int main()
 {
-    int arr[] = {2, 3, 4, 10, 40};
-    int n = sizeof(arr) / sizeof(arr[0]);
-    int x = 10;
-    int result = binarySearch(arr, 0, n - 1, x);
-    (result == -1) ? printf("Element is not present in array")
-                   : printf("Element is present at index %d", result);
-    return 0;
+    int l;
+    printf("Enter number of elements - ");
+    scanf("%d", &l);
+    printf("Enter elements of SORTED array - ");
+    int nums[l];
+    for (int i = 0; i < l; i++)
+    {
+        scanf("%d", &nums[i]);
+    }
+    int target;
+    printf("Enter the target element - ");
+    scanf("%d", &target);
+    int index = binary_search(nums, 0, l - 1, target);
+    if(index != -1) 
+    printf("Index - %d", index);
+    else
+    printf("Element not found!");
 }
